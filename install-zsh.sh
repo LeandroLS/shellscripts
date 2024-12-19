41273c0bd82c0ae93da4e3d54540a229049cb327
Leandro Lima <llimasilva@proton.me>
2024-12-18 22:01:02 -0300

    changing script to install node

--- install-zsh.sh
+++ install-zsh.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # Função para exibir mensagens formatadas
@@ -34,3 +35,37 @@ else
   RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || die "Falha ao instalar Oh My Zsh."
 fi
 
+# Instalar NVM (Node Version Manager)
+info "Instalando NVM..."
+if [ -d "$HOME/.nvm" ]; then
+  info "NVM já está instalado."
+else
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash || die "Falha ao instalar o NVM."
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \ . "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \ . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
+
+# Instalar Node.js 22
+info "Instalando Node.js 22..."
+source "$HOME/.nvm/nvm.sh"
+nvm install 22 || die "Falha ao instalar Node.js 22."
+
+# Verificar versão do Node.js e npm
+info "Verificando instalação do Node.js e npm..."
+NODE_VERSION=$(node -v 2>/dev/null)
+NPM_VERSION=$(npm -v 2>/dev/null)
+
+if [[ "$NODE_VERSION" == v22* ]]; then
+  info "Node.js $NODE_VERSION instalado com sucesso."
+else
+  die "Falha ao verificar a instalação do Node.js."
+fi
+
+if [[ -n "$NPM_VERSION" ]]; then
+  info "npm $NPM_VERSION instalado com sucesso."
+else
+  die "Falha ao verificar a instalação do npm."
+fi
+
+info "Configuração concluída com sucesso!"
